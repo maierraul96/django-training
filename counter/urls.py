238237ee9e7f7c5f9f67486143ba73100957a174
maierraul96 <maierraul96@gmail.com>
2020-04-01 10:17:31 +0300

    Working buttons

--- counter/urls.py
+++ counter/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'counter'
 urlpatterns = [
+    path('/increment', views.increment, name='increment'),
+    path('/decrement', views.decrement, name='decrement'),
     path('', views.index, name='index')
 ]
--- counter/views.py
+++ counter/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Counter
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse(Counter.objects.first())
+    counter = Counter.objects.first()
+    context = {'counter': counter}
+    return render(request, 'counter/index.html', context)
+
+
+def increment(request):
+    counter = Counter.objects.first()
+    counter.increment()
+    counter.save()
+    return HttpResponseRedirect(reverse('counter:index'))
+
+
+def decrement(request):
+    counter = Counter.objects.first()
+    counter.decrement()
+    counter.save()
+    return HttpResponseRedirect(reverse('counter:index'))
+
+
+def count(request):
+    print(request.POST)
+    return HttpResponseRedirect(reverse('counter:index'))

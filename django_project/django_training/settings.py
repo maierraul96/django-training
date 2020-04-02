2f9215ffdff145d72ab6f9971e86d655d7de0a23
maierraul96 <maierraul96@gmail.com>
2020-04-02 15:18:02 +0300

    Add environment variables

--- django_project/django_training/settings.py
+++ django_project/django_training/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
+print(os.environ.get("DJANGO_ALLOWED_HOSTS"))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
--- django_project/django_training/wsgi.py
+++ django_project/django_training/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
+project_folder = os.path.abspath(os.path.join(__file__, "../../.."))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_training.settings')
+load_dotenv(os.path.join(project_folder, '.env.dev'))
 
 application = get_wsgi_application()

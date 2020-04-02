b2cd564e1c3cb8f8b7f94ed2be674f37116f6882
maierraul96 <maierraul96@gmail.com>
2020-04-02 11:33:44 +0300

    Working container

--- django_project/django_training/settings.py
+++ django_project/django_training/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ouo95ae066(+yz-whm$&l@=2$iblla)69)rcp#brlt$8*hcuki'
+# SECRET_KEY = 'ouo95ae066(+yz-whm$&l@=2$iblla)69)rcp#brlt$8*hcuki'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
---
+++ start-server.sh
@@ -0,0 +1,1 @@
+#!/bin/shpython /opt/app/django_training/manage.py runserver 0.0.0.0:8000
